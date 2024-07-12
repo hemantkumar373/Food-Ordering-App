4b9315f448a1bdfec703e034aaa2537cafab3da5
Hemant Kumar <123319697+hemantkumar373@users.noreply.github.com>
2024-07-13 00:36:01 +0530

    Update Home.jsx

--- Frontend/src/components/Home/Home.jsx
+++ Frontend/src/components/Home/Home.jsx
@@ -2,9 +2,20 @@ import React, { useState } from 'react'
 import MenuItems from '../MealItems'
 import Selection from '../PassengerList'
 import Meals from '../SelectMeal'
+import { useAuthContext } from '../../context/AuthContext'
 
 const Home = () => {
   const [data, setData] = useState(null)
+	const { setAuthUser } = useAuthContext();
+
+  const handleLogout = async () => {
+    await fetch("http://localhost:3000/api/auth/logout", {
+      method: "POST",
+    });
+
+    setAuthUser(null);
+
+  }
   return (
     <div className='flex w-full h-full overflow-x-hidden'>
       <MenuItems data={data} setData={setData} className="flex-shrink-0 max-w-[300px] overflow-y-auto" />
@@ -13,6 +24,7 @@ const Home = () => {
         <div className="flex flex-col">
           <Meals className="p-4" />
         </div>
+      <button className='bg-[#1D9BF0] border-none hover:bg-blue-500 py-2 my-4 rounded-full text-lg text-white btn' onClick={handleLogout}>Logout</button>
       </div>
     </div>
   )
