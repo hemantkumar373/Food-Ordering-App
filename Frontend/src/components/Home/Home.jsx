38f09218b04f3c9d22614ab502ec5b4c95232633
Hemant Kumar <123319697+hemantkumar373@users.noreply.github.com>
2024-07-13 15:33:27 +0530

    Update Home.jsx

--- Frontend/src/components/Home/Home.jsx
+++ Frontend/src/components/Home/Home.jsx
@@ -1,30 +1,29 @@
 import React, { useState } from 'react'
-import MenuItems from '../MealItems'
-import Selection from '../PassengerList'
-import Meals from '../SelectMeal'
 import { useAuthContext } from '../../context/AuthContext'
+import MealItems from '../MealItems'
+import SelectMeal from '../SelectMeal'
+import PassengerList from '../PassengerList'
 
 const Home = () => {
   const [data, setData] = useState(null)
-	const { setAuthUser } = useAuthContext();
+  const { setAuthUser } = useAuthContext();
 
   const handleLogout = async () => {
     await fetch("http://localhost:3000/api/auth/logout", {
       method: "POST",
     });
-
+	  
     setAuthUser(null);
-
   }
   return (
     <div className='flex w-full h-full overflow-x-hidden'>
-      <MenuItems data={data} setData={setData} className="flex-shrink-0 max-w-[300px] overflow-y-auto" />
+      <MealItems data={data} setData={setData} className="flex-shrink-0 max-w-[300px] overflow-y-auto" />
       <div className="flex flex-col">
-        <Selection className="mb-4" />
+        <PassengerList className="mb-4" />
         <div className="flex flex-col">
-          <Meals className="p-4" />
+          <SelectMeal className="p-4" />
         </div>
-      <button className='bg-[#1D9BF0] border-none hover:bg-blue-500 py-2 my-4 rounded-full text-lg text-white btn' onClick={handleLogout}>Logout</button>
+        <button className='bg-blue-500 border-none hover:bg-blue-600 py-2 my-4 rounded-full text-lg text-white btn' onClick={handleLogout}>Logout</button>
       </div>
     </div>
   )
